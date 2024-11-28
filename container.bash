54cf6276f613950a1d66943fb7753ba3343d4a60
Alexandre Ferro <alexandreacff@gmail.com>
2024-11-28 12:12:53 -0300

    Update container.bash

--- container.bash
+++ container.bash
@@ -6,11 +6,8 @@ docker run \
         --ipc=host \
 	--net host \
 	--volume /dev:/dev \
-	--device /dev/bus/usb \
-	--device /dev/snd \
 	--privileged \
 	--name residencia_container \
         --volume "$PWD:/workspace" \
 	--gpus all  \
-	macall:riva_stream
-#	pytorch/pytorch:2.3.1-cuda12.1-cudnn8-devel
+	pytorch/pytorch:2.3.1-cuda12.1-cudnn8-devel
